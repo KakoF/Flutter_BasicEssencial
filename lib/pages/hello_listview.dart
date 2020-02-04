ddb060db520659c2d59ea8fe67d1437864e7e7ca
Marcos <kakoferrare87@gmail.com>
2020-02-04 19:31:47 -0300

    exemplo de StatefulWidget alternando item de objeto em listView e GridView, redesenhando a view

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -9,9 +9,14 @@ class Tecnologia {
   Tecnologia(this.nome, this.foto);
 }
 
-class HelloListView extends StatelessWidget {
+class HelloListView extends StatefulWidget {
 
-  bool gridView = true;
+  @override
+  _HelloListViewState createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+  bool _gridView = true;
 
   @override
   Widget build(BuildContext context) {
@@ -20,10 +25,16 @@ class HelloListView extends StatelessWidget {
         title: Text('Lista de Itens'),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.list),onPressed: (){
-            this.gridView = false;
+            print('lista');
+            setState(() {
+              this._gridView = false;
+            });
           }),
           IconButton(icon: Icon(Icons.grid_on),onPressed: (){
-            this.gridView = true;
+            print('Grid');
+            setState(() {
+              this._gridView = true;
+            });
           })
         ],
       ),
@@ -39,30 +50,43 @@ class HelloListView extends StatelessWidget {
       Tecnologia('Tecnologia 4','assets/images/javascript.png'),
     ];
 
-    return ListView.builder(
-          itemCount: tecs.length,
-          itemExtent: 300,
-          itemBuilder: (context, index){
-            Tecnologia tec = tecs[index];
-            return Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-               _img(tec.foto),
-                Container(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                      margin: EdgeInsets.all(12),
-                        padding: EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        color: Colors.black45,
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                        child: Text(tec.nome, style: TextStyle(fontSize: 26, color: Colors.white))
-                    )
-                )
-              ],
-            );
-      });
+   if(_gridView){
+     return GridView.builder(
+         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2) ,
+         itemCount: tecs.length,
+         itemBuilder: (context, index){
+           return _itemView(tecs, index);
+         });
+   }else{
+     return ListView.builder(
+         itemCount: tecs.length,
+         itemExtent: 300,
+         itemBuilder: (context, index){
+           return _itemView(tecs, index);
+         });
+   }
+  }
+
+  Stack _itemView(List<Tecnologia> tecs, int index) {
+    Tecnologia tec = tecs[index];
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+       _img(tec.foto),
+        Container(
+            alignment: Alignment.topLeft,
+            child: Container(
+              margin: EdgeInsets.all(12),
+                padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.black45,
+                borderRadius: BorderRadius.circular(16),
+              ),
+                child: Text(tec.nome, style: TextStyle(fontSize: 26, color: Colors.white))
+            )
+        )
+      ],
+    );
   }
 
   _img(String caminho) {
